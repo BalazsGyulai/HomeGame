10d8e73d7fc96759393122e857b7660d75798114
Gyulai Balázs <trombita0531@gmail.com>
2022-07-06 13:59:38 +0200

    chart.js proba

---
+++ src/components/Data.js
@@ -0,0 +1,14 @@
+export const UserData = [
+    {
+        id: 1,
+        year: 2016,
+        userGain: 80000,
+        userLost: 823,
+    },
+    {
+        id: 2,
+        year: 2017,
+        userGain: 90000,
+        userLost: 325,
+    }
+]
--- src/pages/PlayerInfo.js
+++ src/pages/PlayerInfo.js
@@ -2,13 +2,19 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import "./PlayerInfo.css";
+import { Chart , PointElement, LineElement, CategoryScale, LinearScale} from "chart.js";
+import { Line } from "react-chartjs-2";
+
+Chart.register(
+    PointElement, LineElement, CategoryScale, LinearScale
+)
 
 const PlayerInfo = () => {
   const { id } = useParams();
   const [username, setUsername] = useState("");
-  const [ scores, setScores ] = useState(0);
-  const [ loses, setLoses] = useState(0);
-  const [ winPoints, setWinPoints] = useState(0);
+  const [scores, setScores] = useState(0);
+  const [loses, setLoses] = useState(0);
+  const [winPoints, setWinPoints] = useState(0);
 
   useEffect(() => {
     UpgradeUsername();
@@ -27,7 +33,6 @@ const PlayerInfo = () => {
     axios
       .get(`http://localhost/players.php/?players=5&user=${id}`)
       .then((res) => {
-        console.log(res.data);
         setScores(res.data);
       });
   };
@@ -38,24 +43,20 @@ const PlayerInfo = () => {
 
   function Losses() {
     axios
-    .get(`http://localhost/players.php/?players=7&user=${id}`)
-    .then((res) => {
-      console.log(res.data);
-      setLoses(res.data);
-    //   setScores(res.data);
-    });
-    return <div>{loses}</div>
+      .get(`http://localhost/players.php/?players=7&user=${id}`)
+      .then((res) => {
+        setLoses(res.data);
+      });
+    return <div>{loses}</div>;
   }
 
   function Wins() {
     axios
-    .get(`http://localhost/players.php/?players=8&user=${id}`)
-    .then((res) => {
-      console.log(res.data);
-      setWinPoints(res.data);
-    //   setScores(res.data);
-    });
-    return <div>{winPoints}</div>
+      .get(`http://localhost/players.php/?players=8&user=${id}`)
+      .then((res) => {
+        setWinPoints(res.data);
+      });
+    return <div>{winPoints}</div>;
   }
 
   return (
@@ -78,6 +79,32 @@ const PlayerInfo = () => {
             <AllGames />
           </div>
         </div>
+        <Line
+          options={{
+            responsive: true,
+            plugins: {
+                title: {
+                    display: true,
+                    text: "Egy",
+                }
+            }
+          }}
+        data = {{
+            labels: ['Day 1', 'Day 2', 'Day 3', 'Day 4', 'Day 5', 'Day 6'],
+            datasets: [
+              {
+                label: 'Dataset',
+                data: [10,20,30,20,30,10],
+                borderColor: "rgb(255,0,0)",
+                backgroundColor: "rgba(255,0,0,0.2)",
+                pointStyle: 'circle',
+                pointRadius: 10,
+                pointHoverRadius: 15
+              }
+            ]
+        }}
+
+        />
       </div>
     </>
   );
